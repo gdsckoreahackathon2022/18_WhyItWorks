226274bf79fa8f8219a826dd49cb3f4e112afad4
sk000514 <sk000514@gist.ac.kr>
2022-02-05 09:25:03 +0900

    lsg

--- App.js
+++ App.js
@@ -4,17 +4,43 @@ import * as React from 'react';
 import ViewImage from './components/1'
 import DataFetch from './components/dataFetch';
 import { NavigationContainer } from '@react-navigation/native'; 
-import { createStackNavigator } from '@react-navigation/stack';
-const Stack =createStackNavigator();
+import { createStackNavigator } from '@react-navigation/stack'; 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tab';
+import { Tab } from 'react-native-elements/dist/tab/Tab';
+const MapStack =createStackNavigator();
+const UserStack =createStackNavigator();
+const Tab =createBottomTabNavigator();
+
+function MapScreen() {
+  return(
+      <MapStack.Navigator initialRouteName='home'>
+        <MapStack.Screen name='home' component={/*추후 추가(지도)*/}/>
+        <MapStack.Screen name='all_photo' component={DataFetch}/>
+        <MapStack.Screen name='detail' component={ViewImage}/>        
+      </MapStack.Navigator>
+  )
+}
+
+function UserScreen() {
+  return(
+    <UserStack.Navigator initialRouteName='signin'>
+        <UserStack.Screen name='signin' component={SignInForm}/>
+        <UserStack.Screen name='signup' component={SignUpForm}/>
+        <UserStack.Screen name='mypage' component={MyPage}/>        
+      </UserStack.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='first'>
-        <Stack.Screen name='start' component={DataFetch}/>
-        <Stack.Screen name='details' component={ViewImage}/>        
-      </Stack.Navigator>
-    </NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Map' component={MapScreen}/>
+        <Tab.Screen name='User' component={UserScreen}/>
+        <Tab.Screen name='Camera' component={CameraScreen}/>
+        <Tab.Screen name='Ranking' component={RankingScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>      
   );
 }
 
